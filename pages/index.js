5b9eb12cd44105f40147e879f8a45ec2f7939274
Rony Vilela <50499337+ronyrvilela@users.noreply.github.com>
2023-07-05 19:17:25 +0000

    altera texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Frase Alterada</h1>
+    return <h1>Querida Lalá, Você foi escolhida para o meu primeiro desafio. Essa é minha primeira página e é dedicada a você. bjim! se gostou, dá uma risadinha</h1>
 }
 
 export default Home; 
